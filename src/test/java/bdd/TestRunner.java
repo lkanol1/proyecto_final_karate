f40f05c53a00ceda0bd2c24df927d71ec554a664
Jose Mallcco Ayala <joskanol@gmail.com>
2026-07-31 23:19:06 -0500

    Update QA pipeline with tag filter5

--- src/test/java/bdd/TestRunner.java
+++ src/test/java/bdd/TestRunner.java
@@ -12,4 +12,19 @@ public class TestRunner {
     Karate testRegister() {
         return Karate.run("registerAuth").relativeTo(getClass());
     }
+
+    @Karate.Test
+    Karate testAddProduct() {
+        return Karate.run("product/addProduct").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testUpdateProduct() {
+        return Karate.run("product/updateProduct").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testGetProduct() {
+        return Karate.run("product/get_by_product").relativeTo(getClass());
+    }
 }
